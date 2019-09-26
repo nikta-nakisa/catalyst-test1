7fb5b35b55e30302bb89193ae1237b3e96ad728c
unknown <nikta.nakisa@ose.com.au>
2019-09-27 01:10:48 +1000

    Catalyst test 1 * Final changes: ** Make sure nothing is commited to database when an email is invalid ** Added Database functionality

--- user_upload.php
+++ user_upload.php
@@ -25,8 +25,6 @@
     );
     $user_options = getopt($options, $long_options);
 
-    $file_name = $user_options['file'];
-
     // Check if help option exists, echo directive and descriptions
     if (isset($user_options['help'])) {
         echo($help_directives);
@@ -34,36 +32,50 @@
     }
     // Create users table and exit
     if (isset($user_options['create_table'])) {
-        $conn = database_manager();
-
-        die();
+        die(create_database_table());
     }
 
     if (!isset($user_options['file'])) {
-        die('Please specify your csv file name');
+        die("Please specify your csv file name.\n");
     }
     $file_name = $user_options['file'];
     $lines = file_content_handler($file_name);
+    //removing headers from CSV file
+    array_shift($lines);
+    $insert_record_flag = isset($user_options['dry_run']) ? false : true;
+
+    // Make sure users table exist
+    if ($insert_record_flag) {
+        $conn = database_connector();
+        $conn->begin_transaction();
+        create_database_table($conn);
+    }
 
     foreach ($lines as $line) {
         $name = $surname = $email = '';
         $fields = explode(',', $line);
 
-        $name = pares_data($fields[0], 'NAME');
+        $name    = parse_data($fields[0], 'NAME');
         $surname = parse_data($fields[1], 'NAME');
-        $email = parse_data($fields[2], 'EMAIL');
-
-        if (!isset($user_options[''])) {
-            // Insert new record into users tables
+        if (!($email = parse_data($fields[2], 'EMAIL'))) {
+            if ($insert_record_flag) {
+                $conn->rollback();
+            }
+            die("Invalid email $fields[2] in CSV file.\n");
         }
+
+        if ($insert_record_flag) {
+            // Insert new record into users table
+            insert_new_record($name, $surname, $email, $conn);
+        }
+    }
+    if ($insert_record_flag) {
+        $conn->commit();
+        $conn->close();
     }
 
 
-
     // Open the file and parse it
-
-    // Check if records should be added to database or not
-
     function file_content_handler($file_name) {
         $lines = file($file_name, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         if (empty($lines)) {
@@ -75,7 +87,6 @@
     function parse_data($data, $type) {
         $parsed_data = '';
         $data = trim($data);
-        $name_pattern = '/[!@#$%^&*0-9]/';
         switch ($type) {
             case 'NAME':
                 $parsed_data = ucfirst(strtolower($data));
@@ -83,7 +94,7 @@
                 break;
             case 'EMAIL':
                 if (filter_var($data, FILTER_VALIDATE_EMAIL) === false) {
-                    die("Invalid email address : $data in CSV file");
+                    return false;
                 }
                 $parsed_data = $data;
                 break;
@@ -93,19 +104,60 @@
         return $parsed_data;
     }
 
-    function database_manager() {
+    function database_connector() {
+        global $user_options;
         $user_name = $user_options['u'] ?? '';
-        $password = $user_options['p']  ?? '';
+        $password  = $user_options['p'] ?? '';
         $host_name = $user_options['h'] ?? '';
 
         if (empty($user_name) || empty($password) || empty($host_name)) {
             die("MySql Connection details are not provided\n");
         }
-        $conn = new mysqli($host_name, $user_name, $password);
-
-    // Check connection
-        if ($conn->connect_error) {
+        $conn = new mysqli($host_name, $user_name, $password, 'homestead', '3306');
+        // Check database connection
+        if (!$conn) {
             die("Connection failed: " . $conn->connect_error);
         }
+
         return $conn;
+    }
+
+    function create_database_table($conn = null) {
+        $close_connection_flag = false;
+
+        if (is_null($conn)) {
+            $conn = database_connector();
+            $close_connection_flag = true;
+        }
+        $result = $conn->query('CREATE TABLE IF NOT EXISTS c_users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            surname VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )');
+        if ($close_connection_flag) {
+            $conn->close();
+        }
+        if ($result) {
+            return("users table is created \n");
+        } else {
+            return($conn->error . "\n");
+        }
+    }
+
+    function Insert_new_record($name, $surname, $email, $conn = null) {
+        $close_connection_flag = false;
+
+        if (is_null($conn)) {
+            $conn = database_connector();
+            $close_connection_flag = true;
+        }
+        $query = 'INSERT INTO c_users(name,surname,email) VALUES("' . $name . '","' . $surname . '","' . $email . '")';
+        $conn->query($query);
+
+        if ($close_connection_flag) {
+            $conn->close();
+        }
+
     }
